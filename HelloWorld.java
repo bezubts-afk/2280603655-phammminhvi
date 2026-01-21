a1e9f4ba512e6fb94a53e3132dffa4089b93e18c
Tên của bạn <email@example.com>
2026-01-21 11:23:22 +0700

    Xóa HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-// File này không được sử dụng trong bài tập JavaScript
-// Nếu cần, có thể xóa hoặc chỉnh sửa
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Bài tập Quản Lý Sản Phẩm - JavaScript");
-    }
-}
